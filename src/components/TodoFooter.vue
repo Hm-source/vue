f3fcfdfc887848b09ed99538c5d78a835434a3e2
Hm-source <swo06090vip@hufs.ac.kr>
2022-07-13 11:37:29 +0900

    feat: add TodoFooter component

---
+++ src/components/TodoFooter.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="clearAllTodos">
+    <button @click="clearAll">clear all</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    clearAll() {
+      localStorage.clear()
+    },
+  },
+}
+</script>

d2701d2e41829b4a28bb9671b98afbd6a8bb6345
Hm-source <swo06090vip@hufs.ac.kr>
2022-07-13 11:08:12 +0900

    feat: add TodoInput Component

---
+++ src/components/TodoInput.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <input type="text" :key="newTodoItem" />
+    <button @click="addTodo" @keyup.enter="addTodo">add todo</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      newTodoItem: "",
+    }
+  },
+  methods: {
+    addTodo() {
+      if (this.newTodoItem === "") {
+        alert("내용을 입력하세요.")
+      } else {
+        console.log(this.newTodoItem)
+        localStorage.setItem(this.newTodoItem, this.newTodoItem)
+        this.newTodoItem = ""
+      }
+    },
+  },
+}
+</script>
